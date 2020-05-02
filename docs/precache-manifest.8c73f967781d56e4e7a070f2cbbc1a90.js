f2fa343e8f538bbc18e5e3318876bd7c0f3cfb14
Nitin Kumar <wmnitin@gmail.com>
2020-05-02 16:05:12 +0530

    deploy build

---
+++ docs/precache-manifest.8c73f967781d56e4e7a070f2cbbc1a90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a851dd02607afc5f497d66f20bc2ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24bb346fa3702932fbc5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5320e362c67fa7bb6517",
+    "url": "/static/js/2.1fdf0465.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fdf0465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24bb346fa3702932fbc5",
+    "url": "/static/js/main.00e217be.chunk.js"
+  },
+  {
+    "revision": "b64defa02034728a3970",
+    "url": "/static/js/runtime-main.906748a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
